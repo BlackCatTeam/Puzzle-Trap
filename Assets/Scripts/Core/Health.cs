2776584940b44a8c9608da674f2cfe426973b536
Gabriel Arcasa <gabriel.arcasa97@gmail.com>
2022-01-27 01:44:04 -0300

    Add Barbarian

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BlackCat.Core {
+namespace BlackCat.Core {	
 	public class Health : MonoBehaviour
 	{
 		[SerializeField]
